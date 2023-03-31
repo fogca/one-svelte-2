6d4d2a106db54727ccbdea9ff4552a40954d36e3
takumiisobe <hi@takumiisobe.com>
2023-03-31 15:20:56 +0900

    0331_1501

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,32 +4,27 @@
   import PageTransition from '../components/PageTransition.svelte';
   import Header from '../components/Header.svelte'
   import Footer from '../components/Footer.svelte'
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    window.setTimeout(function(){
+      //FONTPLUS.start();
+      FONTPLUS.reload(false)
+    }, 100);
+  });
+
 
 	/** @type {import('./$types').LayoutData} */
 	export let data;
-
   $: currentRoute = $page.url.pathname; 
 </script>
 
 <svelte:head>
-  <link rel="stylesheet" href="../css/base.css?var=1.13">
+  <link rel="stylesheet" href="../css/base.css?var=1.14">
   <link rel="stylesheet" href="../css/main.css?var=1.13">
   <link rel="stylesheet" href="../css/prerendering.css?var=1.13">
-  <link rel="stylesheet" href="https://use.typekit.net/frp6udl.css">
-  <script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?A2b35yD5mnk%3D&box=qnyUbRZ0KZQ%3D&aa=1&ab=2"></script><!--one fontplus-->>
+  <script src="https://webfont.fontplus.jp/accessor/script/fontplus.js?A2b35yD5mnk%3D&box=qnyUbRZ0KZQ%3D&aa=1&ab=2"></script><!--one fontplus-->
   <script src="https://unpkg.com/colcade@0/colcade.js"></script>
-  <!--
-  <script>
-    (function(d) {
-      var config = {
-        kitId: 'rlj3snt',
-        scriptTimeout: 3000,
-        async: true
-      },
-      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-    })(document);
-  </script>
-  -->
 </svelte:head>
 
 <div 
